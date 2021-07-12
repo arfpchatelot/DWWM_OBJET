2ea04d92f84027f585d85a2d51d66c3e0f582e93
arfpchatelot <fchatelot@yahoo.fr>
2021-07-12 16:47:39 +0200

    Amelioration projet compte et compte rémunéré

---
+++ Lib_financiers/Banque.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace financier
+{
+    public class Banque
+    {
+        //attributs 
+        public Compte[] mesComptes = new Compte[30];
+
+        private int nbComptes;
+        private string nomBanque;
+        private string villeBanque;
+
+        //constructeurs
+        public Banque(string _nom, string _ville)
+        {
+            this.nomBanque = _nom;
+            this.villeBanque = _ville;
+            this.NbComptes = 0;
+        }
+        //propriétés
+        public string NomBanque { get => NomBanque; }
+        public string VilleBanque { get => villeBanque; }
+        public int NbComptes { get => nbComptes; set => nbComptes = value; }
+
+
+        // methodes
+        private void AjouterCompte(Compte _autreCompte)
+        {
+            nbComptes++;
+            mesComptes[(nbComptes - 1)] = _autreCompte;
+        }
+        public bool AjouterCompte(string _nom, int _numero, double _solde, double _decouvert)
+        {
+
+
+            for (int i = 0; i < nbComptes; i++)
+            {
+                if (mesComptes[i].Numero == _numero)
+                {
+                    return false;
+
+                }
+            }
+            Compte _compteTemp = new Compte(_numero, _nom, _solde, _decouvert);
+            this.AjouterCompte(_compteTemp);
+            return true;
+
+        }
+
+
+        public override string ToString()
+        {
+            string chaine = "-------------------- BANQUE " + this.nomBanque + "//" + this.villeBanque + "------------------------\n ---------------------liste des comptes------------------------ \n ";
+            for (int i = 0; i < this.nbComptes; i++)
+            {
+                chaine += "\n" + mesComptes[i].ToString() + "------------------- \n";
+            }
+            chaine += "------------------ fin de liste --------------------------\n";
+            return chaine;
+
+        }
+
+        public Compte RendCompte(int _numeroCompte)
+        {
+            for (int i = 0; i < this.nbComptes; i++)
+            {
+                if (mesComptes[i].Numero == _numeroCompte)
+                {
+                    return mesComptes[i];
+                }
+            }
+            return null;
+        }
+        public Compte RendCompte2(int _numeroCompte)
+        {
+            bool test = false;
+            int indice = 0;
+            for (int i = 0; i < this.nbComptes; i++)
+            {
+                if (mesComptes[i].Numero == _numeroCompte)
+                {
+                    test = true;
+                    indice = i;
+
+                }
+            }
+            if (test)
+            {
+                return mesComptes[indice];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Compte CompteSup()
+        {
+            Compte comptemax = this.mesComptes[0];
+            for (int i = 1; i < nbComptes; i++)
+            {
+                if (mesComptes[i].Solde > comptemax.Solde)
+                {
+                    comptemax = mesComptes[i];
+                }
+
+            }
+            return comptemax;
+
+
+
+        }
+    }
+}
---
+++ Lib_financiers/Compte_remu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace financier
+{
+      public class Compte_Remu:Compte
+    {
+       public static double taux_Interet=0.02;
+
+       private DateTime date_Ouverture;
+       private  List<double> tabSoldesCompte = new List<double>();
+
+       public Compte_Remu(int _numero, string _nomCli, double _solde, double _decouvert, int _jour, int _mois,int _annee) : base(_numero, _nomCli, _solde, _decouvert)
+        {
+            this.date_Ouverture = new DateTime(_annee, _mois, _jour);
+
+            for (int i = 0; i < 24; i++)
+            {
+                tabSoldesCompte.Add(_solde);
+            }
+                
+      
+        }
+        public override string ToString()
+        {
+            return base.ToString()+ "\n pour taux d'intérêt :"+ taux_Interet +" et pour date d'ouverture "+ this.date_Ouverture.ToString("dd-MM-yyyy") ;
+        }
+        public double  Calculer_Interet( DateTime _uneDate)
+        {
+
+
+            int temp;
+            if (_uneDate.Day>15)
+            {
+                 temp = _uneDate.Month * 2 - 1;
+            }
+            else
+            {
+                temp = _uneDate.Month * 2 -2 ;
+
+            }
+                double sommeinterets = 0;
+            for (int i =0 ; i < temp; i++)
+            {
+               sommeinterets += this.tabSoldesCompte[i]*taux_Interet/(double)24;
+            }
+
+           
+            return sommeinterets;
+        }
+        
+
+
+    }
+}
